e079032e1e515e835e1bd5dfa894ac430bca1e41
seanmcgovern21 <seanmcgovern21@gmail.com>
2020-04-17 19:19:16 -0400

    Update add-noise.py

--- add-noise.py
+++ add-noise.py
@@ -1,1 +1,19 @@
+import soundfile as sf
+import numpy as np
 
+def add_noise(data_new, samplerate, data_noise, samplerate_noise):
+
+	j=0
+	for i in range(len(data_new)):
+		data_new[i] = data_new[i] + data_noise[j,0]*6
+		if samplerate < samplerate_noise:
+			j=j+2
+		else:
+			j=j+1
+
+	
+
+	sf.write('new_file.flac', data_new, samplerate)
+
+	return(data)
+	
