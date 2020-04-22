b96e1b7805898c06a989d9a6cb4f9caf7f3f5272
Aqueous98 <33072976+Aqueous98@users.noreply.github.com>
2020-04-21 20:27:39 -0400

    added basic skeleton of network
    basics of training and prediction also implemented

--- data-processing.py
+++ data-processing.py
@@ -6,39 +6,59 @@ import add-echoes as ae
 import add-noise as an
 import preprocessing as pp
 import random
+import tensorflow as tf
+
+from tensorflow.keras import layers
 
 corpus = pra.datasets.CMUArcticCorpus(download=True, speaker=['bdl', 'slt'])
 
-#create network here
+#the two outermost layers need to be dense with the dimensionality of the input/output
+model = tf.keras.Sequential()
+model.add(layers.Dense(50))
+model.add(layers.LSTM(64))
+model.add(layers.Dense(50))
 
-for i in len(corpus)*(3/4):
+def loss(target, pred):
+  return tf.keras.losses.MSE(target, pred)
+model.compile(optimizer='adam', loss=loss)
+
+samples = corpus
+targets = corpus
+
+for i in len(corpus):
     sample = corpus[i].data
     echosample = ae.add_echoes(sample)
     #add noise here
     #add echoes and noise here
-    target = pp.process_sentence(sample, 16000)
+    targets[i] = pp.process_sentence(sample, 16000)
     
     #randomise which sample is input
     input = 0
     rand = random.randint(0, 3)
     if rand==0:
-        input = sample
+        samples[i] = sample
     elif rand==1:
-        input = echosample
+        samples[i] = echosample
     elif rand==2:
         #add just noise
-        input = sample
+        samples[i] = sample
     else:
         #add both
-        input = sample
-    #train on this sample, maybe spectrogram first
-    
+        samples[i] = sample
+        
+#train the network
+model.fit(samples, targets, epochs = 50)
+
 #test
 input = corpus[len(corpus)-1]
-input.play()
+sounddevice.play(input, 16000)
 plt.figure()
-input.plot()
+plt.plot(input)
 plt.show()
 
 #run the network on the test sample
+output = model.predict(input)
 #plot and play output
+sounddevice.play(output, 16000)
+plt.plot(output)
+plt.show()
