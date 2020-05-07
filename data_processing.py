48093b52770b22197777ad3ecfc8443fad809b12
Dhruv Kool Rajamani <dhruvkoolrajamani@gmail.com>
2020-05-06 22:08:55 -0400

    padding sentences

--- data_processing.py
+++ data_processing.py
@@ -37,15 +37,26 @@ model.compile(optimizer='adam', loss=loss)
 samples = []
 targets = []
 
+max_val = 0
 for i in range(len(corpus)):
-  sample = deepcopy(corpus[i].data.astype(np.float64))
+  if len(corpus[i].data) > max_val:
+    max_val = len(corpus[i].data)
+
+for i in range(len(corpus)):
+  sample = corpus[i].data.astype(np.float64)
   fs = corpus[i].fs
-  echosample = ae.add_echoes(sample)
-  noisesample = an.add_noise(sample, sf.read("RainNoise.flac"))
-  bothsample = ae.add_echoes(noisesample)
+  echosample = librosa.util.fix_length(ae.add_echoes(sample), max_val)
+  noisesample = librosa.util.fix_length(
+    an.add_noise(sample,
+                 sf.read("RainNoise.flac")),
+    max_val
+  )
+  bothsample = librosa.util.fix_length(ae.add_echoes(noisesample), max_val)
+
   # data_out, _ = pp.process_sentence(sample, fs=fs)
   # data_out = deepcopy(sample)
-  targets.append(sample)  # np.abs(ss.stft(data_out, fs=fs, nfft=512))
+  target_fft = np.abs(librosa.core.stft(sample, n_fft=512))
+  targets.append(target_fft)  # np.abs(ss.stft(data_out, fs=fs, nfft=512))
 
   #randomise which sample is input
   rand = random.randint(0, 2)
@@ -67,6 +78,9 @@ for i in range(len(corpus)):
                                n_fft=512,
                                center=True))
     )
+
+samples = np.array(samples)
+targets = np.array(targets)
 
 #train the network
 model.fit(samples, targets, epochs=50)
